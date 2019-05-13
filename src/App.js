a35718b958f26a3cc7802c3e5e55c2d06579dc51
nicoto <toulousenicholas@gmail.com>
2019-05-12 17:54:03 -0700

    version 1 of clicky, renders html in text only not to the DOM as wished

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.images = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg","11.jpg", "12.jpg"];
+    this.state = {
+      score: 0,
+      imgclicked: {
+        '1.jpg': 0, '2.jpg': 0, '3.jpg': 0, '4.jpg': 0, '5.jpg': 0, '6.jpg': 0, '7.jpg': 0, '8.jpg': 0, '9.jpg': 0, '10.jpg': 0, '11.jpg': 0, '12.jpg': 0
+      }
+    }
+  }
+  render(){
+    let html = [];
+
+    this.images.forEach((src, i) => {
+      let row;
+      if(i%4 === 0){
+        html.push(<div class='row'></div>);
+      } else {
+        let a = `./images/${this.images[i]}`;
+        html.push(<div class='square'><img src={a} alt='pixar character' /></div>);
+      }
+    });
+
+  return (
+    <div className="App container">
+      <header>This is the header</header>
+        {html}
+
+      <footer>This is the footer</footer>
+    </div>
+  );
+}
+}
+
+export default App;
